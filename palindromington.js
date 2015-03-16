f7f0c205f3fe65a04d9a121ee11d6f7dc02b83aa
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-15 22:38:06 -0400

    recursive method of palindrome detection without reverse

--- palindromington.js
+++ palindromington.js
@@ -1,10 +1,25 @@
 var word1 = "lionoil"
 
-function palindrome (word){
+function palindrome (word) {
 	charlist = word.split('');
-	if charlist == charlist.reverse();{
+	reverse_list = charlist.reverse();
+	if (charlist == reverse_list) {
 		return true;
 	} else {
 		return false;
 	}
+}
 
+function palindromeAlt(word) {
+    charlist = word.split(''); // returns list of char
+    console.log(charlist);
+    if (charlist[0] != charlist[charlist.length - 1]){
+    	return false;
+		}else if (charlist.length == 1){
+				return true;
+		}else{
+			charlist.shift();
+			charlist.pop();
+      return palindromeAlt(charlist.join(''););
+      }
+    }
