7e3d4e64a93b87c639fb6f53d9436f4675bf9b57
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-19 21:18:54 -0400

    fixed spacing

--- utopian_tree.js
+++ utopian_tree.js
@@ -1,12 +1,11 @@
-function uTree (cycles){
-	var tree = 1;
-	for (var i = 0; i < cycles; i++){
-		if (i % 2 == 0 || i == 0){
-			tree += tree;
-		} else {
-			tree += 1;
-		}
-	}
-return tree;
+function uTree(cycles) {
+    var tree = 1;
+    for (var i = 0; i < cycles; i++) {
+        if (i % 2 == 0 || i == 0) {
+            tree += tree;
+        } else {
+            tree += 1;
+        }
+    }
+    return tree;
 }
-
