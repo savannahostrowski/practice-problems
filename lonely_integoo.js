33fa8518870601f5443d6090451eec5e1f0dd1c0
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-19 20:21:43 -0400

    fixed whitespace on lonely_integoo.js and initial commit for utopian tree

--- lonely_integoo.js
+++ lonely_integoo.js
@@ -1,20 +1,20 @@
 function processData(input) {
-	var newList = [];
-	var counter = {};
-	for (var i = 0; i < input.length; ++i) {
-  	if (counter[input[i]] === undefined) {
-  		counter[input[i]] = 1;
-		} else {
-  		counter[input[i]] += 1;
-		}
-  }
-  for (var key in counter){
-  	if (counter.hasOwnProperty(key))
-  		if (counter[key] == 1){
-  		newList.push(parseInt(key));
-  	}
-  }
-  return newList;
+    var newList = [];
+    var counter = {};
+    for (var i = 0; i < input.length; ++i) {
+        if (counter[input[i]] === undefined) {
+            counter[input[i]] = 1;
+        } else {
+            counter[input[i]] += 1;
+        }
+    }
+    for (var key in counter) {
+        if (counter.hasOwnProperty(key)){
+        } if (counter[key] == 1) {
+                newList.push(parseInt(key));
+            }
+    }
+    return newList;
 }
 
-console.log(processData([1,1,1,2,2,3,3,4,5,5]));
+console.log(processData([1, 1, 1, 2, 2, 3, 3, 4, 5, 5]));
---
+++ utopian_tree.js
@@ -0,0 +1,12 @@
+function utopianTree (cycles){
+	var tree = 1;
+	var cyclesToCalc = Array.apply(null, {length: cycles}).map(Number.call, Number);
+	for (var i = 0; i < cyclesToCalc.length; i++){
+		if (cyclesToCalc[i] % 2 == 0 || cyclesToCalc[i] == 0){
+			tree += tree;
+		} else {
+			tree += 1;
+		}
+	}
+return tree;
+}
