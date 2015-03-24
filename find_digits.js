158c6d4658b1b9fc61b8e32966c45472b67cbc37
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-24 19:31:34 -0400

    removed unnecessary stuff

--- find_digits.js
+++ find_digits.js
@@ -2,9 +2,7 @@ function findDigits(n) {
   num_divide = 0;
   number_to_str = String(n);
   for (var i = 0; i < number_to_str.length; i++) {
-    if (number_to_str[i] == 0) {
-      num_divide += 0;
-    } else {
+    if (number_to_str[i] != 0) {
       remainder = n % Number(number_to_str[i])
       if (remainder == 0) {
         num_divide += 1;
