b5f24af0a822be88dea528cde503d4e14581e1a5
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-23 21:52:43 -0400

    initial commit for find_digits

---
+++ find_digits.js
@@ -0,0 +1,16 @@
+function findDigits (n){
+	num_divide = 0;
+	number_to_str = String(n);
+	for (var i = 0; i < number_to_str.length; i++){
+		if (number_to_str[i] == 0){
+			num_divide += 0;
+		} else {
+			remainder = n % Number(number_to_str[i])
+			if (remainder == 0){
+				num_divide += 1 ;
+			}
+		}
+	}
+	return num_divide;
+}
+
