27c9a8196a6f33a83c3a30695563911fee4293c3
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-23 22:00:08 -0400

    wow, indentation

--- find_digits.js
+++ find_digits.js
@@ -1,16 +1,15 @@
-function findDigits (n) {
-	num_divide = 0;
-	number_to_str = String(n);
-	for (var i = 0; i < number_to_str.length; i++){
-		if (number_to_str[i] == 0){
-			num_divide += 0;
-		} else {
-			remainder = n % Number(number_to_str[i])
-			if (remainder == 0){
-				num_divide += 1 ;
-			}
-		}
-	}
-	return num_divide;
+function findDigits(n) {
+  num_divide = 0;
+  number_to_str = String(n);
+  for (var i = 0; i < number_to_str.length; i++) {
+      if (number_to_str[i] == 0) {
+          num_divide += 0;
+      } else {
+          remainder = n % Number(number_to_str[i])
+          if (remainder == 0) {
+              num_divide += 1;
+          }
+      }
+  }
+  return num_divide;
 }
-
