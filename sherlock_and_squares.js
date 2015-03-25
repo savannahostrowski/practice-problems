d260abefb170ba0b8e9ba06a396d9769bf5a0fde
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-24 23:10:52 -0400

    omg sublime

--- sherlock_and_squares.js
+++ sherlock_and_squares.js
@@ -1,9 +1,9 @@
 function sherlockAndSquares(num1, num2) {
-  var arr = [];
-  for (var i = num1; i <= num2; i++) {
-  	if ((Math.sqrt(i) % 1) === 0) {
-      arr.push(i);
-    }
-  }
-  return arr.length;
+	var arr = [];
+	for (var i = num1; i <= num2; i++) {
+		if ((Math.sqrt(i) % 1) === 0) {
+			arr.push(i);
+		}
+	}
+	return arr.length;
 }
