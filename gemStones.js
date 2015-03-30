b5ba87c83797399724df319f7a7b40ccd5056b4e
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-03-29 20:57:24 -0400

    fixed whitespace

--- gemStones.js
+++ gemStones.js
@@ -5,20 +5,20 @@ function gemStones(composition) {
   var gemElems = 0;
   // sort compositions
   for (var i = 0; i < composition.length; i++) {
-      toArray = composition[i].split('');
-      sort = toArray.sort();
-      sortedComps.push(sort);
+    toArray = composition[i].split('');
+    sort = toArray.sort();
+    sortedComps.push(sort);
   }
   //get uniques
   for (var i = 0; i < sortedComps.length; i++) {
-      lst = [];
-      str = sortedComps[i];
-      for (var j = 0; j < str.length; j++) {
-          if (str[j] != str[j + 1]) {
-              lst.push(str[j]);
-          }
+    lst = [];
+    str = sortedComps[i];
+    for (var j = 0; j < str.length; j++) {
+      if (str[j] != str[j + 1]) {
+        lst.push(str[j]);
       }
-      uniques.push(lst);
+    }
+    uniques.push(lst);
   }
 
   var merged = [];
@@ -26,15 +26,15 @@ function gemStones(composition) {
   var elements = uniques.pop();
 
   for (var i = 0; i < elements.length; i++) {
-      var count = 0;
-      for (var j = 0; j < merged.length; j++) {
-          if (elements[i] === merged[j]) {
-              count += 1;
-          }
+    var count = 0;
+    for (var j = 0; j < merged.length; j++) {
+      if (elements[i] === merged[j]) {
+        count += 1;
       }
-      if (count === sortedComps.length) {
-          gemElems += 1;
-      }
+    }
+    if (count === sortedComps.length) {
+      gemElems += 1;
+    }
   }
   return gemElems;
 }
