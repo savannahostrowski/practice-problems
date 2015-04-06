dfe98b240360d156fa03faf6ddab055f9255fa7a
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-04-06 15:19:29 -0700

    fixed 1,1000 case

--- maximized_xor.js
+++ maximized_xor.js
@@ -1,10 +1,17 @@
-function maximizedXor (num1, num2){
-	arr = [];
-	for (var i = num1; i <= num2; i++){
-		for (var j= num1; j <= num2; j++){
-			arr.push(i ^ j);
-		}
-	}
-	return Math.max.apply( Math, arr );;
+function maximizedXor(num1, num2) {
+  var arr = [];
+  for (var i = num1; i <= num2; i++) {
+    for (var j = num1; j <= num2; j++) {
+      arr.push(i ^ j);
+    }
+  }
+  var maxSoFar = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxSoFar) {
+      maxSoFar = arr[i];
+    }
+  }
+  return maxSoFar;
 }
-		
+
+console.log(maximizedXor(1, 1000));
