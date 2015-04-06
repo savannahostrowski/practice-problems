d5325c25cb54c5240551aa3470769d7a6f723372
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-04-05 22:13:20 -0700

    palindrome index done

--- palindromeIndex.js
+++ palindromeIndex.js
@@ -1,14 +1,31 @@
-function palindromeIndex(word){
-  charlist = word.split('');
-  for (var i = 0; i < charlist.length; i++){
-  	var newList = charlist;
-  	newList.splice(i, 1);
-  	console.log(newList, newList.reverse());
-  	if (newList === newList.reverse()){
-  		return i;
-  	} else {
-  		return -1;
-  	}
-  }
+function arraysEqual(arr1, arr2) {
+    var count = 0;
+    for (var j = 0; j < arr1.length; j++) {
+        if (arr1[j] === arr2[j]) {
+            count += 1;
+        }
+    }
+    if (count === arr1.length) {
+        return true;
+    }
+    return false;
 }
-console.log(palindromeIndex('aaab'));
+
+function palindromeIndex(word) {
+    var charlist = word.split('');
+    if (charlist === charlist.reverse()){
+      return -1;
+    }
+    for (var i = 0; i < charlist.length; i++) {
+        var arr = charlist.slice(0);
+        arr.splice(i, 1);
+        var reverse = arr.slice(0).reverse();
+        if (arraysEqual(arr, reverse)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+console.log(palindromeIndex('aaa'));
