a76d79b48343e0cfe0c9d97c13f908c38f1f22e6
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-05-20 12:08:40 -0400

    final solution for mxn matrix

--- replaceWith0.js
+++ replaceWith0.js
@@ -4,18 +4,32 @@
 function replaceWith0(matrix) {
     var numCols = matrix[0].length;
     var numRows = matrix.length;
-    var index;
+    var index = [];
+    // gets row
     for (var i = 0; i < numRows; i++) {
+        // gets element in row
         for (var j = 0; j < numCols; j++) {
-            if (matrix[i][j] === 0) {
-                (matrix[i]).map(Math.sqrt);
-                var index = j;
+            var row = matrix[i];
+            //if row element equals 0
+            if (row[j] === 0) {
+                //map zeros to all elements in row
+                newRow = row.map(function (x) {
+                    return 0;
+                });
+                matrix[i]= newRow;
+                // adds the index to the index array
+                index.push(j);
+                break;
             }
         }
     }
-    if (index != undefined) {
-        for (var i = 0; i < numRows; i++) {
-            matrix[i][index] = 0;
+    // iterates through each value in the index array
+    for (var k = 0; k < index.length; k++) {
+        // for each row in matrix
+        for (var l = 0; l < numRows; l++ ) {
+            var row = matrix[l];
+            //sets element in column equal to 0
+            row[k] = 0;
         }
     }
     console.log(matrix);
@@ -24,6 +38,6 @@ function replaceWith0(matrix) {
 
 replaceWith0([
     [0, 1, 2, 3, 4],
-    [1, 2, 3, 4, 5],
+    [1, 0, 3, 4, 5],
     [2, 3, 4, 5, 6]
 ]);
