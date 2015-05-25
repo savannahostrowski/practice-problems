53fcfcf908626f5650973794b024f5914c4596b3
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-05-24 21:39:11 -0400

    more work - to eventually run on digital ocean

--- checkSite.py
+++ checkSite.py
@@ -1,11 +1,26 @@
-import sys, os, time, httplib2
+import requests, sched, time, smtplib
 
+s = sched.scheduler(time.time, time.sleep)
+site = 'http://www.ostrowski.ca'
 
+def siteCheck(url, sc):
+	print(time.time())
+	r = requests.head(url)
+	if r.status_code == 200:
+		print(r.status_code)
+		emailTextMe()
+	sc.enter(10,1, siteCheck, (sc,))
 
-def checkSite():
-	conn = httplib2.HTTPConnection('www.ostrowski.ca')
-	conn.request("HEAD", "/")
-	response = conn.getresponse()
-	print(response.status, response.reason)
+s.enter(10,1,siteCheck, (s,))
+s.run()
 
-	checkSite()
+def emailTextMe():
+	server = smtplib.SMTP('stmp.gmail.com', 587)
+	server.starttls()
+	server.login('email', 'password')
+	msg = 'Site update: http://www.ostrowski.ca has yielded\
+		   a status other than 200'
+	server.sendmail('email',
+	 				'email', msg)
+	server.sendmail('???', '5199981350@msg.telus.com', msg)
+	server.quit()
