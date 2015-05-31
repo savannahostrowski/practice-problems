c4a64ed730fdd0c4e1a793754907a98a568da77a
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-05-30 23:00:30 -0400

    more work on missing numbers

--- missingNumbers.js
+++ missingNumbers.js
@@ -1,6 +1,7 @@
 function missingNumbers(lst1, lst2){
-  d1 = {}
-  d2 = {}
+  d1 = {};
+  d2 = {};
+  missing = [];
   for (var i = 0; i < lst1.length; i++) {
     if (d1[lst[i]] === undefined) {
       d1[lst[i]] = 1;
@@ -15,5 +16,16 @@ function missingNumbers(lst1, lst2){
       d2[lst[j]] += 1;
     }
   }
-  
+  for (var key1 in d1) {
+    for (var key2 in d2) {
+      if (key1 == key2){
+        if (d1[key1] != d2[key2]) {
+          diff = abs(d1[key1] - d2[key2]);
+          for (var m = 0; m < diff; m++) {
+            missing.push(key1);
+          }
+        }
+      }
+    }
+  }
 }
