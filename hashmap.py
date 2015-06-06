7e1cfc071a9e011f0ea78698f4e0e819f6f8e757
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-06-06 18:02:58 -0400

    more hashmap learning

--- hashmap.py
+++ hashmap.py
@@ -8,11 +8,24 @@ class HashMap:
         self.hashmap = [None] * 7
 
     def hash(self, key):
-        pass
-
-    def get(self, key):
-        pass
-
+        return key % len(self.hashmap)
+    
     def set(self, key, value):
         index = self.hash(key)
-        self.hashmap[index] = E(key, value)
+        i = 0
+        while i < len(self.hashmap) and self.hashmap[(index + i) % len(self.hashmap)] is not None:
+            i += 1
+        self.hashmap[index + i] = E(key, value)
+
+    def get(self, key):
+        index = self.hash(key)
+        i = 0
+        while True:
+            if i >= len(self.hashmap):
+                break
+            j = (index + i) % len(self.hashmap)
+            if self.hashmap[j] is None:
+                raise KeyError('Not found')
+            if self.hashmap[j].key == key:
+                return self.hashmap[j].value
+            i += 1
