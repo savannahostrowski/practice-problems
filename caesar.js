bd6ce2d3f53f84b9a35ad41db9b9fb28e9bfa6f0
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-07-02 23:27:10 -0400

    almost done

--- caesar.js
+++ caesar.js
@@ -1,11 +1,16 @@
 function caesarCipher(str, k) {
   var newLetterList = [];
+  var finalWord = "";
   for (var i = 0; i < str.length; i++) {
     if (typeof str[i] != "symbol") {
       var charVal = str.charCodeAt(i);
       var newVal = charVal + k;
       newLetterList.push (newVal);
     }
-
+  for (var i = 0; i < newLetterList.length; i++) {
+    var converted = newLetterList.charCodeAt(i);
+    finalWord += converted;
+  }
 }
+return finalWord;
 }
