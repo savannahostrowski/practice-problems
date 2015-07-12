22aafb0f274b5ef865bb63ba2d202bc62ae2c076
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-07-11 21:55:15 -0400

    whatever

--- sherlockAndArray.js
+++ sherlockAndArray.js
@@ -2,15 +2,17 @@ function sherlockAndArray (arr) {
   var subArr = [];
   var total = 0;
   for (var i = 0; i < arr.length; i++) {
-    if (subArr.length !== 0) {
+      if (subArr.length !== 0) {
       for (var j = 0; j < subArr.length; j++) {
         total += subArr[j];
       }
       if (total === str[i]) {
-        return True;
+        console.log('true');
       } else {
         total = 0;
       }
     }
+  }
 }
-}
+
+console.log(sherlockAndArray([1,2,3,6,5,7]));
